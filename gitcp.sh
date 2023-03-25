df07f6763c8e53c3fdd3c5df5d30c9c4d6063b9e
Yadav106 <sahil.jp.yadav@gmail.com>
2023-03-25 22:51:51 +0530

    Added branch name inclusion to gitcp.sh script

--- gitcp.sh
+++ gitcp.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "$1" # $1 is the first argument passed to the script, pass the commit message
-git push origin main
+git commit -m "$2" # $2 is the second argument passed to the script, pass the commit message
+git push origin $1 # $1 is the first argument passed to the script, pass the branch name
